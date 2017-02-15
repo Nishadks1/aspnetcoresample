f509291af53fb536c9a47395727a24ce70ecf5dd
Christian Nagel <christian@christiannagel.com>
2017-02-15 21:33:41 +0100

    ASP.NET MVC sample

---
+++ ASPNETMVCSample/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BooksLib;
+
+namespace ASPNETMVCSample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+
+        public IActionResult TagHelpers() =>
+            View(new Book { Title = "enter a title" });
+
+        [HttpPost]
+        public string UploadBook(Book b) =>
+            $"book {b.Title} from {b.Publisher} uploaded";
+
+        public IActionResult Books1() =>        
+            ViewComponent("BooksList", "Wrox Press");
+
+        public IActionResult Books2() =>
+            View();
+
+        public IActionResult Books3() =>
+            View();
+    }
+}
---
+++ ASPNETMVCSample/Services/BooksSampleService.cs
@@ -0,0 +1,24 @@
+﻿using BooksLib;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ASPNETMVCSample.Services
+{
+    public class BooksSampleService : IBooksService
+    {
+        private readonly List<Book> _books = new List<Book>();
+        public BooksSampleService() => InitBooks();
+
+        private void InitBooks()
+        {
+            _books.AddRange(new Book[]
+            {
+                new Book { BookId = 1, Title = "Professional C# 6 and .NET Core 1.0", Publisher = "Wrox Press"},
+                new Book { BookId = 2, Title = "Enterprise Services", Publisher = "AWL"},
+                new Book { BookId = 3, Title = "Professional C# 5 and .NET 4.5.1", Publisher = "Wrox Press"}
+            });
+        }
+        public Task<IEnumerable<Book>> GetBooksAsync() =>
+            Task.FromResult<IEnumerable<Book>>(_books);
+    }
+}
